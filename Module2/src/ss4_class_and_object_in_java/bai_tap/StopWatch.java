e2ba70e0955da8cd9af8ab0f684b1ea535b3173a
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-01-05 14:15:11 +0700

    ss4

--- Module2/src/ss4_class_and_object_in_java/bai_tap/StopWatch.java
+++ Module2/src/ss4_class_and_object_in_java/bai_tap/StopWatch.java
@@ -1,2 +1,23 @@
-package ss4_class_and_object_in_java.bai_tap;public class StopWatch {
+package ss4_class_and_object_in_java.bai_tap;
+
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+
+    public StopWatch(){
+         this.startTime=  new Date();
+
+    }
+    public Date setStarTime(){
+      return   this.startTime=new Date();
+    }
+    public Date getEndTime(){
+        return  this.endTime=new Date();
+    }
+
+
+
+
 }
