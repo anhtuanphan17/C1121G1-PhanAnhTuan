c770a85db1d286b23e5e12eba42e991043966989
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-01-20 14:24:21 +0700

    update1

--- Module2/src/ss15_exception_and_debug/bai_tap/IllegalTriangleException.java
+++ Module2/src/ss15_exception_and_debug/bai_tap/IllegalTriangleException.java
@@ -1,12 +1,12 @@
 package ss15_exception_and_debug.bai_tap;
 
 public class IllegalTriangleException extends Exception {
-    super.
 
+    public IllegalTriangleException() {
+    }
 
-
-
-
-
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
 
 }
