f0b6b024e3fe2f14f2b89bb1263e245fe5d774ad
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-03-02 08:43:58 +0700

    update

--- Module3/ss5_view_index_stored_procedure/thuc_hanh/stored_procedure.sql
+++ Module3/ss5_view_index_stored_procedure/thuc_hanh/stored_procedure.sql
@@ -1,5 +1,4 @@
 
-
 DELIMITER //
 
 CREATE PROCEDURE findAllCustomers()
@@ -27,3 +26,23 @@ SELECT * FROM customers where customerNumber = 175;
 
 END //
 DELIMITER ;
+
+
+DELIMITER //
+drop procedure Setcounter;
+CREATE PROCEDURE SetCounter(INOUT counter INT,IN inc INT)
+
+BEGIN
+	set counter = 5;
+    SET counter = counter+inc;
+
+END//
+DELIMITER ;
+
+
+SET @counter =2;
+call Setcounter (@counter,1); 
+-- call Setcounter (@counter,1);
+select @counter;
+
+
