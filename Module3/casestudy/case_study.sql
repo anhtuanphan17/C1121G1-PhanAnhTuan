810cb5dee4c2d97ce63e1d86fb4a8f228cb6db24
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-03-04 10:59:50 +0700

    update

--- Module3/casestudy/case_study.sql
+++ Module3/casestudy/case_study.sql
@@ -289,7 +289,200 @@ inner join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tie
 where loai_khach.ma_loai_khach = 1 and (dia_chi like '%Vinh%' or dia_chi like '%Quảng Ngãi%');
 
 -- task 12:
+
+select hop_dong.ma_hop_dong, nhan_vien.ho_ten, khach_hang.ho_ten, khach_hang.so_dien_thoai,dich_vu.ten_dich_vu, 
+sum(ifnull(hop_dong_chi_tiet.so_luong,0)) as `so_luong_dich_vu_di_kem`, hop_dong.tien_dat_coc from hop_dong
+left join khach_hang on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+left join nhan_vien on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+left join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where hop_dong.ma_hop_dong in (select hop_dong.ma_hop_dong from hop_dong where (month(ngay_lam_hop_dong) between 10 and 12) and year(ngay_lam_hop_dong)=2020)
+and hop_dong.ma_hop_dong not in (select hop_dong.ma_hop_dong from hop_dong where (month(ngay_lam_hop_dong) between 1 and 6) and year(ngay_lam_hop_dong)=2021)
+group by hop_dong.ma_hop_dong; 
  
+
+-- task 13:
+select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem, sum(hop_dong_chi_tiet.so_luong) as so_lan_su_dung from dich_vu_di_kem
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+group by dich_vu_di_kem.ma_dich_vu_di_kem
+having so_lan_su_dung = (select sum(hop_dong_chi_tiet.so_luong) as so_lan_su_dung from dich_vu_di_kem
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+group by dich_vu_di_kem.ma_dich_vu_di_kem order by so_lan_su_dung desc limit 1);
+
+-- task 14:
+select hop_dong.ma_hop_dong,loai_dich_vu.ten_loai_dich_vu, dich_vu_di_kem.ten_dich_vu_di_kem,count(dich_vu_di_kem.ma_dich_vu_di_kem) as so_lan_su_dung from dich_vu_di_kem
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+join hop_dong on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+join loai_dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
+group by dich_vu_di_kem.ma_dich_vu_di_kem 
+having so_lan_su_dung =1
+order by so_lan_su_dung;
+
+-- task 15:
+select nhan_vien.ma_nhan_vien, nhan_vien.ho_ten, trinh_do.ten_trinh_do, bo_phan.ten_bo_phan, 
+nhan_vien.so_dien_thoai,nhan_vien.dia_chi, hop_dong.ngay_lam_hop_dong
+from nhan_vien
+left join trinh_do on trinh_do.ma_trinh_do =nhan_vien.ma_trinh_do
+left join bo_phan on bo_phan.ma_bo_phan = nhan_vien.ma_bo_phan
+left join hop_dong on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+where year(hop_dong.ngay_lam_hop_dong) between 2020 and 2021
+group by nhan_vien.ma_nhan_vien
+having  count(hop_dong.ma_hop_dong) <=3 
+order by ma_nhan_vien ;
+
+-- task 16: 
+select  nhan_vien.ma_nhan_vien, nhan_vien.ho_ten from nhan_vien where nhan_vien.ma_nhan_vien not in
+(select nhan_vien.ma_nhan_vien from nhan_vien
+left join hop_dong on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+where year(hop_dong.ngay_lam_hop_dong) between 2019 and 2021
+group by nhan_vien.ma_nhan_vien);
+
+-- delete:
+create temporary table temp_hop_dong_xoa_nhan_vien(
+select hop_dong.ma_nhan_vien from hop_dong
+inner join nhan_vien on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien 
+where year(hop_dong.ngay_lam_hop_dong) between 2019 and 2021
+group by hop_dong.ma_nhan_vien
+);
+
+
+set sql_safe_update =0;
+delete from nhan_vien where nhan_vien.ma_nhan_vien not in (select * from temp_hop_dong_xoa_nhan_vien);
+set sql_safe_update =1; 
+
+
+-- task 17:
+-- lay thong tin
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ma_loai_khach, sum(ifnull(dich_vu.chi_phi_thue,0) + ifnull(hop_dong_chi_tiet.so_luong,0) * ifnull(dich_vu_di_kem.gia,0)) as tong_tien from khach_hang
+left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+left join hop_dong on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+left join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem =hop_dong_chi_tiet.ma_dich_vu_di_kem
+group by khach_hang.ma_khach_hang
+having tong_tien > 10000000 and loai_khach.ma_loai_khach = 2;
+
+-- update
+
+UPDATE khach_hang,
+    (SELECT 
+        khach_hang.ma_khach_hang
+    FROM
+        khach_hang
+    LEFT JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+    LEFT JOIN hop_dong ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+    LEFT JOIN dich_vu ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+    LEFT JOIN hop_dong_chi_tiet ON hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+    LEFT JOIN dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+    GROUP BY khach_hang.ma_khach_hang
+    HAVING SUM(IFNULL(dich_vu.chi_phi_thue, 0) + (IFNULL(hop_dong_chi_tiet.so_luong, 0) * IFNULL(dich_vu_di_kem.gia, 0))) > 10000000) AS temp 
+SET 
+    khach_hang.ma_loai_khach = 1
+WHERE
+    khach_hang.ma_loai_khach = 2
+        AND khach_hang.ma_khach_hang = temp.ma_khach_hang;
+        
+	
+    
+-- cach 2:
+update khach_hang set khach_hang.ma_loai_khach =1
+where khach_hang.ma_loai_khach =2
+and khach_hang.ma_khach_hang in (select hop_dong.ma_khach_hang from hop_dong
+join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+group by hop_dong.ma_khach_hang
+having sum(ifnull(dich_vu.chi_phi_thue, 0) + (ifnull(hop_dong_chi_tiet.so_luong, 0) * ifnull(dich_vu_di_kem.gia, 0))) > 10000000 );
+
+
+-- task 18:
+select khach_hang.ma_khach_hang, khach_hang.ho_ten from khach_hang
+join hop_dong on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+where year(hop_dong.ngay_lam_hop_dong) < 2021;
+
+-- delete
+create temporary table temp_khach_hang(
+select khach_hang.ma_khach_hang from khach_hang
+inner join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang where year(hop_dong.ngay_lam_hop_dong) < 2021
+);
+
+delete from hop_dong_chi_tiet where hop_dong_chi_tiet.ma_hop_dong 
+in (select hop_dong.ma_hop_dong from hop_dong where hop_dong.ma_khach_hang in(select ma_khach_hang from temp_khach_hang));
+
+delete from hop_dong where hop_dong.ma_khach_hang in (select ma_khach_hang from temp_khach_hang);	
+
+delete from khach_hang where khach_hang.ma_khach_hang in (select ma_khach_hang from temp_khach_hang);
+
+
+-- task 19:
+update dich_vu_di_kem set dich_vu_di_kem.gia = dich_vu_di_kem.gia*2;
+
+select  dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem, count(hop_dong_chi_tiet.ma_dich_vu_di_kem) as so_lan_su_dung from dich_vu_di_kem
+left join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+left join hop_dong on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+-- where year(hop_dong.ngay_lam_hop_dong) =2020 
+group by dich_vu_di_kem.ma_dich_vu_di_kem
+having so_lan_su_dung >10;
+
+select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem, dich_vu_di_kem.gia
+from hop_dong_chi_tiet
+join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+join hop_dong on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+where hop_dong_chi_tiet.so_luong > 10 and year(hop_dong.ngay_lam_hop_dong) = 2020;
+
+
+-- task 20:
+select nhan_vien.ma_nhan_vien as id, nhan_vien.ho_ten,nhan_vien.email, nhan_vien.so_dien_thoai, nhan_vien.ngay_sinh, nhan_vien.dia_chi,'nhan vien' as role
+from nhan_vien
+union
+select khach_hang.ma_khach_hang as id, khach_hang.ho_ten, khach_hang.email, khach_hang.so_dien_thoai, khach_hang.ngay_sinh,khach_hang.dia_chi,
+'khach hang' as role
+from khach_hang;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
