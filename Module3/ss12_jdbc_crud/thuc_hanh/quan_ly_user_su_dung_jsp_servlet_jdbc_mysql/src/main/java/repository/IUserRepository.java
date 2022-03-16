2cc5c65cf4639805e9c92b64be41ca09c2378067
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-03-16 16:20:35 +0700

    update

--- Module3/ss12_jdbc_crud/thuc_hanh/quan_ly_user_su_dung_jsp_servlet_jdbc_mysql/src/main/java/repository/IUserRepository.java
+++ Module3/ss12_jdbc_crud/thuc_hanh/quan_ly_user_su_dung_jsp_servlet_jdbc_mysql/src/main/java/repository/IUserRepository.java
@@ -1,8 +1,11 @@
 package repository;
 
-import java.sql.SQLException;
+import model.User;
 
-public interface IUserDAO {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserRepository {
     public void insertUser(User user) throws SQLException;
 
     public User selectUser(int id);
@@ -13,5 +16,14 @@ public interface IUserDAO {
 
     public boolean updateUser(User user) throws SQLException;
 
+    public List<User> searchByCountry (String country) throws SQLException;
+
+    public List<User> sortByName();
+
+    User getUserById(int id);
+
+    void insertUserStore(User user) throws SQLException;
+
+    void addUserTransaction(User user, int[] permision);
 
 }
